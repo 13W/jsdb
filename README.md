3b69d8ea0b1079f4277b2e92ffbe8f0c6a0efc04
Vladimir Bulyga <xx@ccxx.cc>
2016-10-21 19:00:24 +0300

    fast

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
-* JavaScript implementation of MongoDB
+#### JSDB
 
-[![Bitdeli Badge](https://d2weczhvl823v0.cloudfront.net/13W/jsdb/trend.png)](https://bitdeli.com/free "Bitdeli Badge")
+##### Very fast array filter
 
+Test results:
+```
+$ node test
+Complete loading data set, length: 18801
+Request time: Mingo: 122.072ms
+Counts: 81
+Request time: Bout Compiled: 126.694ms
+Counts: 81
+Request time: Sift Compiled: 33.379ms
+Counts: 81
+Request time: JsDB: 16.611ms
+Counts: 81
+Request time: Direct Filter: 5.562ms
+Counts: 81
+Complete!
+```
+
+Benchmark results:
+```
+$ node benchmark
+{"num":{"$lt":5}} x 10 items x 104,438 ops/sec ±2.69% (94 runs sampled)
+{"num":{"$lt":5}} x 40 items x 35,609 ops/sec ±2.01% (92 runs sampled)
+{"num":{"$lt":5}} x 120 items x 11,029 ops/sec ±1.45% (96 runs sampled)
+{"num":{"$lt":5}} x 320 items x 4,389 ops/sec ±1.45% (97 runs sampled)
+{"num":{"$lt":5}} x 800 items x 1,752 ops/sec ±1.67% (95 runs sampled)
+{"num":{"$lt":5}} x 1920 items x 766 ops/sec ±1.36% (94 runs sampled)
+{"num":{"$lt":5}} x 4480 items x 322 ops/sec ±1.63% (88 runs sampled)
+{"num":{"$lt":5}} x 10240 items x 142 ops/sec ±1.60% (83 runs sampled)
+{"num":{"$lt":5}} x 23040 items x 52.89 ops/sec ±1.64% (70 runs sampled)
+```
--- test.js
+++ test.js
@@ -1,142 +1,86 @@
-#!/usr/local/bin/node
-var clone = require('native-deep-extend').clone;
-var array = [],
-    searches = [],
-    iter  = 10000;
+const Query = require('./lib/query'),
+    async = require('async'),
+    fs = require('fs'),
+    readline = require('readline'),
+    dataset = [],
+    testQuery = {"category_code" : "web", "founded_year" : {$gte: 2004}, "acquisition.acquired_year": {$gt: 2010}};
 
-function wrap(message, iter,func) {
-    var start = new Date().getTime();
-    for(var i = 0; i < iter; i++) func(i);
-    var end = new Date().getTime(),
-        elapsed = (end-start)/1000;
-    console.log(message, ":", Math.round(iter/elapsed),"op/sec", (elapsed), 'sec');
+const mingo = require('mingo'),
+    sift = require('sift'),
+    Bout = require('mongo-bout').filter;
+
+function loader(cb) {
+    const reader = readline.createInterface({
+        input: fs.createReadStream('./datasets/companies.json')
+    });
+
+    reader.on('line', function (line) {
+        var json = JSON.parse(line);
+        json._id = json._id.$oid;
+        dataset.push(json);
+    });
+
+    reader.on('close', function () {
+        console.log('Complete loading data set, length:', dataset.length);
+        cb();
+    });
 }
 
-function random() {
-    var out = '';
-    for(var i = 0; i < Math.floor(Math.random()*25);i++) out += String.fromCharCode(97+Math.floor(Math.random()*25));
-    return out;
+function startTestJsDB(callback) {
+    var query = new Query(testQuery);
+    console.time('Request time: JsDB');
+    var result = dataset.filter(entry => query.execute(entry));
+    console.timeEnd('Request time: JsDB');
+    console.log('Counts:', result.length);
+    callback();
 }
 
-function BinarySearch(t,A,f)       // t - искомый элемент,
-{                                // A - упорядоченный массив, в котором ищем.
-    var i = 0, j = A.length, k; 
-    while (i < j)
-     { k = Math.floor((i+j)/2);
-       if (t <= f(A,k)) j = k;
-       else i = k+1;
-     }
-    
-    if (f(A,i) == t) return i;     // На выходе индекс искомого элемента.
-    else {
-        console.log(f(A, i), t, f(A,i) == t);
-        return -1;}                // Если искомого элемента нет в массиве, то -1.
-}
-var sortCache = {};
-
-function getValue(object, path) {
-    if (!object) return object;
-    var index = path.indexOf('.');
-    if (!~index) return object[path];
-    var key = path.substr(0, index);
-    return getValue(object[key], path.substr(index+1));
+function startTestBout(callback) {
+    var filter = Bout(testQuery);
+    console.time('Request time: Bout');
+    var result = filter(dataset);
+    console.timeEnd('Request time: Bout');
+    console.log('Counts:', result.length);
+    callback();
 }
 
-function bsearch(array, key, value) {
-    var cache, uniq = {};
-    if (sortCache[key] && sortCache[key].array === array) {
-        cache = sortCache[key];
-    } else {
-        for (var i = 0; i < array.length; i++) {
-            var r = getValue(array[i], key);
-            uniq[r] = uniq[r] || [];
-            uniq[r].push(array[i]);
-        }
-        cache = {
-            keys: Object.keys(uniq),
-            values: uniq,
-            array: array
-        };
-        sortCache[key] = cache;
-        cache.keys.sort(function(x, y) {
-            x = +x || x;y = +y || y;
-            return ((x < y) ? -1 : ((x > y) ? 1 : 0));
-        });
-//        console.log(cache);
-    }
-    var index = BinarySearch(value[key], cache.keys, function(A,i) {return A[i];/*getValue(A[i], key)*/}),
-        cacheKey = cache.keys[index],
-        cacheValues = cache.values[cacheKey];
-        if (!cacheValues) {
-            console.warn(cache, key, value[key], index, cacheKey, cacheValues);
-        }
-    return cacheValues;
+function startTestSift(callback) {
+    var query = sift(testQuery);
+    console.time('Request time: Sift');
+    var result = dataset.filter(entry => query(entry));
+    console.timeEnd('Request time: Sift');
+    console.log('Counts:', result.length);
+    callback();
 }
 
-function qsearch(array, query) {
-    var result = array;
-    for (var key in query) {
-        result = bsearch(result, key, query);
-//        console.log(result);
-    }
-    return result;
+function startTestMingo(callback) {
+    console.time('Request time: Mingo');
+    var count = new mingo.Query(testQuery).find(dataset).count();
+    console.timeEnd('Request time: Mingo');
+    console.log('Counts:', count);
+    callback();
 }
 
-function qnsearch(array, query) {}
+function testDirectFilter(callback) {
+    console.time('Request time: Direct Filter');
+    // "category_code" : "web", "founded_year" : {$gte: 2004}, "acquisition.acquired_year": {$gt: 2010}
+    var result = dataset.filter(entry => entry['category_code'] === 'web' && entry['founded_year'] >= 2004 &&
+        entry.acquisition && entry.acquisition.acquired_year > 2010);
+    console.timeEnd('Request time: Direct Filter');
+    console.log('Counts:', result.length);
+    callback();
+}
 
-wrap('Create array', iter, function() {
-    var str = random(),
-        object = {name: str, num: Math.round(Math.random()*iter)};
-    searches.push(str.substr(1, Math.round(Math.floor(Math.random()*str.length)/2)));
-    array.push(object);
+async.parallel([
+    loader
+], function () {
+    async.series([
+        startTestMingo,
+        startTestBout,
+        startTestSift,
+        startTestJsDB,
+        testDirectFilter
+    ], function () {
+        console.log('Complete!');
+    });
 });
-/*
-wrap('search in array with indexOf', iter, function(i) {
-    for (var n = 0; n < iter; n++) if (array[n].indexOf(searches[i]) >= 0) return;
-//    if (!(array[i].indexOf(searches[i]) >=0)) console.error(array[i], searches[i]);
-    console.error('Fail');
-});
-
-wrap('search in array with RegExp', iter, function(i) {
-    var r = new RegExp(searches[i]);
-    for (var n = 0; n < iter; n++) if (r.test(array[n])) return;
-    console.error('Fail');
-});
-*/
-//wrap('Sort', 1, function() {array.sort()});
-/*
-wrap('indexOf search >>>', iter, function(i) {
-    var value  = array[i],
-        search = array.indexOf(value);
-    if (!(array[search] == value)) console.error(value, i, search, array[search]);
-});
-
-wrap('indexOf search <<<', iter, function(i) {
-    var value  = array[iter-i],
-        search = array.indexOf(value);
-    if (!(array[search] == value)) console.error(value, i, search, array[search]);
-});
-*/
-/*
-wrap('bisection search >>>', iter, function(i) {
-    var value  = array[i],
-        search = bsearch(array, 'name', value);//BinarySearch(value, array);
-    if (!(search[0].name == value.name)) console.error(value, i, search, array[search]);
-//    if (search.length > 1) console.log(search.length);
-});
-*/
-var errors = 0;
-wrap('bisection query search >>>', iter, function(i) {
-    var query  = array[i], //{name: array[i].name},
-        search = qsearch(array, query);//BinarySearch(value, array);
-    if (!(search && search[0].name == query.name)) /*errors++;//*/console.error(query, i, search, array[search]);
-//    if (search.length > 1) console.log(search.length);
-});
-console.log(errors);
-/*
-wrap('bisection search <<<', iter, function(i) {
-    var value  = array[iter-i],
-        search = BinarySearch(value, array);
-    if (!(array[search] == value)) console.error(value, i, search, array[search]);
-});
-*/
